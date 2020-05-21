52dacfe67d5a42e7843b27848cde789653822277
DAZUN XING <dazun.xing@gmail.com>
2020-05-21 22:27:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song 
   attr_accessor :title, :artist 
-  def initialize(title, artist)
-    @title =title
-    @artist =artist
-  end
+  # def initialize(title, artist)
+  #   @title =title
+  #   @artist =artist
+  # end
 end
